0a41b699ad456749560342bb7108bcaa33e1161a
kuran <kuranjp@gmail.com>
2025-05-09 12:01:24 +0900

    Revert "レスポンスの扱いを変更"
    This reverts commit 0472ede69aaca1c1e4d4ca22ba06cf16a9c82d5f.

--- app.py
+++ app.py
@@ -50,4 +50,26 @@ def handle_compose_action():
 
     page = page_response.json()
 
-return "", 200
+
+    return jsonify({
+        "composeExtension": {
+            "type": "result",
+            "attachmentLayout": "list",
+            "attachments": [
+                {
+                    "contentType": "application/vnd.microsoft.card.hero",
+                    "content": {
+                        "title": "BookStackへ送信完了",
+                        "text": f"ページ: {page['name']} を作成しました。",
+                        "buttons": [
+                            {
+                                "type": "openUrl",
+                                "title": "開く",
+                                "value": page["url"]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    })
